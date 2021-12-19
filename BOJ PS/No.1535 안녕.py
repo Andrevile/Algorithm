78467cf917239013fd562ca234e526a1fb0a973d
abg3000 <abg3000@naver.com>
2021-12-19 18:24:37 +0900

    1535 commit

--- BOJ PS/No.1535 안녕.py
+++ BOJ PS/No.1535 안녕.py
@@ -10,20 +10,34 @@ damage = list(map(int,sys.stdin.readline().split(" "))) #0<= <=100
 happy = list(map(int,sys.stdin.readline().split(" ")))  #0<= <=100
 
 
+#
+# def Brute_Force(start, energy,point,depth):
+#     global max_happy
+#     visited[start] = True
+#     if max_happy < point:
+#         max_happy = point
+#     for i in range(N):
+#         if visited[i] == False and energy - damage[i] > 0:
+#             Brute_Force(i,energy - damage[i],point + happy[i],depth+1)
+#
+#
+#
+#
+# max_happy = 0
+#
+# for i in range(N):
+#     visited = [False] * N
+#     if 100 - damage[i] > 0:
+#         Brute_Force(i,100-damage[i],happy[i],0)
+# print(max_happy)
 
 
-def Brute_Force(energy):
-    point = 0
-    for i in range(N):
-        if visited[i] == False:
-            if energy - damage[i] > 0:
-                visited[i] = True
-                point += Brute_Force(energy - damage[i])
-                visited[i] = False
-    return 
+DP = [[0 for i in range(100)] for i in range(N+1)]
+for i in range(1,N+1):
+    for k in range(1,100):
+        if k < damage[i-1]:
+            DP[i][k] = DP[i-1][k]
+        else:
+            DP[i][k] = max(DP[i-1][k] , happy[i-1] + DP[i-1][k-damage[i-1]])
 
-
-
-for i in range(N):
-    visited = [False] * N
-
+print(DP[-1][-1])
