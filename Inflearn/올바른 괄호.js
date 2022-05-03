321ba9f5ddc79b216792f11ecd1adcca677a2428
abg3000 <abg3000@naver.com>
2022-05-03 19:37:23 +0900

    study section6 day1

---
+++ Inflearn/올바른 괄호.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const fs = require("fs");
+const input = fs.readFileSync("./dev/stdin").toString();
+
+let answer = solution(input);
+
+function solution(input) {
+  let stack = [];
+  for (let x of input) {
+    if (x === "(") stack.push(x);
+    else {
+      if (stack[stack.length - 1] === "(") {
+        stack.pop();
+      } else {
+        stack.push(x);
+      }
+    }
+  }
+  return stack.length > 0 ? "NO" : "YES";
+}
+
+console.log(answer);
+
+// function solution(input) {
+//   let answer = "YES";
+//   let stack = [];
+//   for (let x of input) {
+//     if (x === "(") stack.push(x);
+//     else {
+//       if (stack.length === 0) return "NO";
+//       stack.pop();
+//     }
+//     console.log(stack);
+//   }
+//   if (stack.length > 0) return "NO";
+//   return answer;
+// }
