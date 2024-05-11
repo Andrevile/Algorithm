a7fe7a30064ea476a821b60a3f9c7d7926c08b27
abg3000 <abg3000@naver.com>
2024-05-11 11:57:16 +0900

    Greedy

---
+++ Greedy/45. Jump Game II.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function (nums) {
+  if (nums.length === 1) return 0;
+
+  let prev = 0,
+    max = 0,
+    jump = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    console.log(i, prev, max, jump);
+    max = Math.max(max, i + nums[i]);
+
+    if (i === prev) {
+      jump++;
+      prev = max;
+    }
+  }
+
+  return jump;
+};
